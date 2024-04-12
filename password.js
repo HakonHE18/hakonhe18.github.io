dbbabd1623c6a1c6606df979446f1086d1528bd2
HakonHE18 <hakonhe@icloud.com>
2024-04-12 18:09:18 +0200

    Added super hidden code for cv

---
+++ password.js
@@ -0,0 +1,17 @@
+document.getElementById("resumeLink").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("passwordModal").style.display = "block";
+});
+
+document.getElementsByClassName("close")[0].addEventListener("click", function() {
+    document.getElementById("passwordModal").style.display = "none";
+});
+
+document.getElementById("submitPassword").addEventListener("click", function() {
+    var password = document.getElementById("passwordInput").value;
+    if (password === "234") { // Change password here <-----------
+        window.location.href = "cv/cv.pdf";
+    } else {
+        alert("Incorrect password. Please try again.");
+    }
+});
